f5b03d1bb268eb808a9a656c28a86541057e3605
ALSKDJFHGG <2294288093@qq.com>
2025-06-03 10:30:59 +0800

    添加了Employee文件的注释

--- src/main/java/com/example/springboot/entity/Employee.java
+++ src/main/java/com/example/springboot/entity/Employee.java
@@ -3,15 +3,24 @@ package com.example.springboot.entity;
 //实体类
 
 public class Employee {
+    // ID
     private Integer id;
+    // 密码
     private String password;
+    // 用户名
     private String username;
     private String role;
+    // 名称
     private String name;
+    // 性别
     private String sex;
+    // 工号
     private String no;
+    // 年龄
     private Integer age;
+    // 个人介绍
     private String description;
+    // 部门
     private Integer departmentId;
 
 
