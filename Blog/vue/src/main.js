46ef46911787a60a1420f29bdeab93b4d95165da
Mrblackso <2696607854@qq.com>
2025-06-06 22:24:44 +0800

    修改网页图标

--- Blog/vue/src/main.js
+++ Blog/vue/src/main.js
@@ -5,16 +5,17 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-// ✅ 导入封装好的 axios 实例
+// 导入封装好的 axios 实例
 import request from './utils/request'
 
 const app = createApp(App)
 
-// ✅ 正确挂载 $axios 到全局属性
+//  正确挂载 axios 到全局属性
 app.config.globalProperties.$axios = request
 
 app.use(router)
 app.use(ElementPlus, {
     locale: zhCn,
 })
+
 app.mount('#app')
